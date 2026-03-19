9b53bcf39f0d76f5a095f6916ca94d74e440f346
BuckMickShot <bngu0038@student.monash.edu>
2026-03-19 17:32:02 +1100

    chore: remove obsolete commented Python code

--- src/player.rb
+++ src/player.rb
@@ -1,38 +1,3 @@
-# from dataclasses import dataclass, field
-# from typing import Set
-
-
-# @dataclass
-# class Player:
-# 	"""Mutable game state for one participant."""
-
-# 	name: str
-# 	cash: int
-# 	position: int = 0
-# 	owned_property_indexes: Set[int] = field(default_factory=set)
-
-# 	def move(self, steps: int, board_size: int) -> int:
-# 		"""Moves the player and returns the number of times GO is passed."""
-# 		if board_size <= 0:
-# 			raise ValueError("Board size must be greater than zero.")
-
-# 		start_position = self.position
-# 		destination = start_position + steps
-# 		self.position = destination % board_size
-
-# 		passes = destination // board_size
-# 		return passes
-
-# 	def credit(self, amount: int) -> None:
-# 		self.cash += amount
-
-# 	def debit(self, amount: int) -> None:
-# 		self.cash -= amount
-
-# 	@property
-# 	def is_bankrupt(self) -> bool:
-# 		return self.cash < 0
-
 require 'set'
 
 # Mutable game state for one participant.
